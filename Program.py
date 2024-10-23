9b9eac80584287faa4a659b0aa4952ad98f886c9
Vastef <vstefanov@protonmail.com>
2024-10-23 20:46:48 +0300

    First commit

---
+++ Program.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+ID = input("UserID: ")
+FirstName = input("FirstName: ")
+LastName = input("LastName: ")
+Email = input("Email: ")
+
+mysql_connect = mysql.connector.connect(
+    host="localhost",
+    user="python",
+    password="asd",
+    database="pythonProgram"
+
+)
+
+mycursor = mysql_connect.cursor()
+
+sql = "insert into Clients (ID, FirstName, LastName, Email) values (%s, %s, %s, %s)"
+values = (ID, FirstName, LastName, Email)
+mycursor.execute(sql, values)
+
+mysql_connect.commit()
+
+print(mycursor.rowcount, "Values have been Inserted")
